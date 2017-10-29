28d4bf1b2e5edf20a0671477dfcb16f5ac7d410c
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2017-10-29 11:48:52 +0700

    Add some dynamic

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
-import {Message} from './messages/message.model';
-
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message: Message = new Message ('Some Message', 'Stan')
+
 }
