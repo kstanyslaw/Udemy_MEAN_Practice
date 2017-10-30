4b72406bd7b44a1cb6ea78c6e8d502af6edc7212
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2017-10-31 06:48:34 +0700

    Add NgForm

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 import {MessageService} from './message.service';
 import {Message} from './message.model';
@@ -9,8 +10,9 @@ import {Message} from './message.model';
 })
 export class MessageInpuComponent {
   constructor(private messageService: MessageService) {};
-  onSave(value: string){
-    const message = new Message (value, 'Stan');
+  onSubmit(form: NgForm){
+    const message = new Message (form.value.content, 'Stan');
     this.messageService.addMessage(message);
+    form.resetForm();
   }
 }
--- assets/app/messages/message.module.ts
+++ assets/app/messages/message.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 import {MessageListComponent} from './message-list.component';
 import {MessageComponent} from './message.component';
@@ -9,7 +10,8 @@ import {MessageService} from './message.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [
     MessageListComponent,
