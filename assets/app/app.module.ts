5e469d2d965fbb4776b3b3bd855fbe2f52738692
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2017-10-31 08:00:24 +0700

    Add header

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
 import { MessageModule } from './messages/message.module';
 import { AuthenticationModule } from './auth/authentication.module';
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
