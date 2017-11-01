f5bcd5b1c6db628f4479b06e8a3a824e3e7959fe
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2017-11-01 14:34:23 +0700

    Add logout, singup and singin components

--- assets/app/auth/authentication.module.ts
+++ assets/app/auth/authentication.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 
-import {AuthenticationComponent} from './authentication.component';
+import { LogoutComponent } from './logout.component';
+import {SinginComponent} from './singin.component';
+import {SingupComponent} from './singup.component';
 
 @NgModule({
   imports: [],
   declarations: [
-    AuthenticationComponent,
+    LogoutComponent,
+    SinginComponent,
+    SingupComponent
   ],
   providers: [],
   exports: [
-    AuthenticationComponent,
+    LogoutComponent,
+    SinginComponent,
+    SingupComponent
   ]
 })
 export class AuthenticationModule { }
