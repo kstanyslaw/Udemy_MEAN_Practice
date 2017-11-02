2d223d50105fbea7b745cdf73003055341ff6fb3
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2017-11-02 16:49:36 +0700

    Add back-end get messages

--- routes/message.js
+++ routes/message.js
@@ -3,6 +3,24 @@ var router = express.Router();
 
 const Message = require('../models/message');
 
+//get messages
+router.get('/', function(req, res, next){
+  Message.find()
+    .exec(function(err, messages) {
+      if (err) {
+        return res.status(500).json({
+          title: 'An error occured',
+          error: err
+        })
+      }
+      res.status(200).json({
+        message: 'Success',
+        obj: messages
+      });
+    });
+});
+
+//Save message
 router.post('/', function(req, res, next){
   var message = new Message({
     content: req.body.content
@@ -18,7 +36,7 @@ router.post('/', function(req, res, next){
       message: 'Saved message',
       obj: result
     });
-  })
+  });
 });
 
 module.exports = router;
