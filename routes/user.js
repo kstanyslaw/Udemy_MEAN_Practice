51336af24d8bb55041c98ce4792dd13c8d936c90
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2017-11-07 21:19:10 +0700

    Add create user method in back-end

---
+++ routes/user.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var router = express.Router();
+var User = require('../models/user');
+
+
+// Create User
+router.post('/', function(req, res, next) {
+  var user = new User({
+    firstName: req.body.firstName;
+    lastName: req.body.lastName;
+    password: bcrypt.hashSync(req.body.password, 10);
+    email: req.body.email;
+  });
+  user.save(function(err, result){
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occured',
+        error: err
+      })
+    }
+  });
+});
+
+module.exports = router;
