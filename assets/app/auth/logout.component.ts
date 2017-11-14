61a2dd05875dbbc2e86322e29ef36c65af5dbaaf
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2017-11-14 17:44:25 +0700

    Add loggin user out (front-end only)

--- assets/app/auth/logout.component.ts
+++ assets/app/auth/logout.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-logout',
   template: '<hr> <div class="col-md-8 col-md-offset-2"> <button type="button" name="button" class="btn btn-danger" (click)="onLogout()">Logout</button> </div>',
 })
 export class LogoutComponent {
-  onLogout() {};
+  constructor(private authService: AuthService, private router: Router) {};
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/auth', 'singin']);
+  };
 }
