1d42371e0d387dff2fdeb0388df4813a02b318b4
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2017-11-14 15:24:57 +0700

    Add singin user front-end

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -7,12 +7,21 @@ import { User } from './user.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private http: Http) {  }
+  constructor(private http: Http) {  };
+
   singup(user: User) {
     const body = JSON.stringify(user);
     const headers = new Headers({'Content-Type': 'application/json'});
     return this.http.post('http://localhost:3000/user', body, {headers: headers})
     .map((response: Response) => response.json())
     .catch((error: Response) => Observable.throw(error.json()));
-  }
+  };
+
+  singin(user: User) {
+    const body = JSON.stringify(user);
+    const headers = new Headers({'Content-Type': 'application/json'});
+    return this.http.post('http://localhost:3000/user/singin', body, {headers: headers})
+    .map((response: Response) => response.json())
+    .catch((error: Response) => Observable.throw(error.json()));
+  };
 }
